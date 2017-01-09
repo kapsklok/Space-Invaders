bbf80c40052e02c671eb11891192b2dc74a84c43
Cordel Hoff <kapsklok@gmail.clm>
2017-01-09 12:12:54 -0600

    Add comments and slightly streamline copying

--- StageBinariesForCommit.sh
+++ StageBinariesForCommit.sh
@@ -1,24 +1,27 @@
 #/bin/sh
+#set up directory structure for zip file
+mkdir bin
 rm -r bin/*
-mkdir -pv bin/SpaceInvaders/lib
-mkdir -v bin/SpaceInvaders/data
+mkdir -p bin/SpaceInvaders/lib
+mkdir -p bin/SpaceInvaders/data
 
-
-
-pwd
-
-cp -r application.*/lib/* bin/SpaceInvaders/lib/
-cp -r application.linux32/data/* bin/SpaceInvaders/data/
-cp application.linux32/SpaceInvaders bin/SpaceInvaders/SpaceInvaders-linux32.sh
-cp application.linux64/SpaceInvaders bin/SpaceInvaders/SpaceInvaders-linux64.sh
-cp application.linux-armv6hf/SpaceInvaders bin/SpaceInvaders/SpaceInvaders-linux-arm.sh
-cp application.windows32/SpaceInvaders.exe bin/SpaceInvaders/SpaceInvaders-windows32.exe
-cp application.windows32/SpaceInvaders.exe bin/SpaceInvaders/SpaceInvaders-windows64.exe
-
+#copy dependency jars and other data into folder to be zipped
 cd bin
+cp -r application.*/lib/* SpaceInvaders/lib/
+cp -r application.linux32/data/* SpaceInvaders/data/
+
+#copy each executable to the folder
+cp application.linux32/SpaceInvaders SpaceInvaders/SpaceInvaders-linux32.sh
+cp application.linux64/SpaceInvaders SpaceInvaders/SpaceInvaders-linux64.sh
+cp application.linux-armv6hf/SpaceInvaders SpaceInvaders/SpaceInvaders-linux-armv6.sh
+cp application.windows32/SpaceInvaders.exe SpaceInvaders/SpaceInvaders-windows32.exe
+cp application.windows32/SpaceInvaders.exe SpaceInvaders/SpaceInvaders-windows64.exe
+
+#zip it up
 zip -r SpaceInvaders.zip ./SpaceInvaders
 
+#delete the folder that has been zipped and the other compiled versions
+#leaving only the zip with all versions included
 rm -r SpaceInvaders
-
 cd ..
 rm -r application.*
