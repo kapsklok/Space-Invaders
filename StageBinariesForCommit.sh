2d17a5efac20d40b9b480a8dede7b16299cbc8fa
Cordel Hoff <kapsklok@gmail.com>
2017-01-08 20:39:17 -0600

    Create folder to hold all files inside the zip

--- StageBinariesForCommit.sh
+++ StageBinariesForCommit.sh
@@ -1,18 +1,18 @@
 #/bin/bash
-mkdir bin
+mkdir -p bin/SpaceInvaders
 cd bin
 rm -r ./*
 cd ..
 
 cp -r application.linux32/lib bin/
 cp -r application.linux32/data bin/
-cp application.linux32/SpaceInvaders bin/SpaceInvaders-linux32.sh
-cp application.linux64/SpaceInvaders bin/SpaceInvaders-linux64.sh
-cp application.linux-armv6hf/SpaceInvaders bin/SpaceInvaders-linux-arm.sh
-cp application.windows32/SpaceInvaders.exe bin/SpaceInvaders-windows32.exe
-cp application.windows32/SpaceInvaders.exe bin/SpaceInvaders-windows64.exe
+cp application.linux32/SpaceInvaders bin/SpaceInvaders/SpaceInvaders-linux32.sh
+cp application.linux64/SpaceInvaders bin/SpaceInvaders/SpaceInvaders-linux64.sh
+cp application.linux-armv6hf/SpaceInvaders bin/SpaceInvaders/SpaceInvaders-linux-arm.sh
+cp application.windows32/SpaceInvaders.exe bin/SpaceInvaders/SpaceInvaders-windows32.exe
+cp application.windows32/SpaceInvaders.exe bin/SpaceInvaders/SpaceInvaders-windows64.exe
 
 cd bin
-zip -r SpaceInvaders.zip ./*
+zip -r SpaceInvaders.zip ./SpaceInvaders
 
 rm -r data lib *.sh *.exe
