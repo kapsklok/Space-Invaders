e6449f36cae7efb546daca8a40d9c497783569ae
Cordel Hoff <kapsklok@gmail.clm>
2017-01-09 19:51:48 -0600

    Fixed bug created in binary generating toolchain

--- StageBinariesForCommit.sh
+++ StageBinariesForCommit.sh
@@ -1,21 +1,21 @@
 #/bin/sh
 #set up directory structure for zip file
-mkdir bin
+mkdir -p bin
 rm -r bin/*
 mkdir -p bin/SpaceInvaders/lib
 mkdir -p bin/SpaceInvaders/data
 
 #copy dependency jars and other data into folder to be zipped
 cd bin
-cp -r application.*/lib/* SpaceInvaders/lib/
-cp -r application.linux32/data/* SpaceInvaders/data/
+cp -r ../application.*/lib/* SpaceInvaders/lib/
+cp -r ../application.linux32/data/* SpaceInvaders/data/
 
 #copy each executable to the folder
-cp application.linux32/SpaceInvaders SpaceInvaders/SpaceInvaders-linux32.sh
-cp application.linux64/SpaceInvaders SpaceInvaders/SpaceInvaders-linux64.sh
-cp application.linux-armv6hf/SpaceInvaders SpaceInvaders/SpaceInvaders-linux-armv6.sh
-cp application.windows32/SpaceInvaders.exe SpaceInvaders/SpaceInvaders-windows32.exe
-cp application.windows32/SpaceInvaders.exe SpaceInvaders/SpaceInvaders-windows64.exe
+cp ../application.linux32/SpaceInvaders SpaceInvaders/SpaceInvaders-linux32.sh
+cp ../application.linux64/SpaceInvaders SpaceInvaders/SpaceInvaders-linux64.sh
+cp ../application.linux-armv6hf/SpaceInvaders SpaceInvaders/SpaceInvaders-linux-armv6.sh
+cp ../application.windows32/SpaceInvaders.exe SpaceInvaders/SpaceInvaders-windows32.exe
+cp ../application.windows32/SpaceInvaders.exe SpaceInvaders/SpaceInvaders-windows64.exe
 
 #zip it up
 zip -r SpaceInvaders.zip ./SpaceInvaders
